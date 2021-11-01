f414d6f2d7c16e72930ef5e632b7ca2da393bf01
Dozerhank <65521931+Dozerhank@users.noreply.github.com>
2021-11-01 09:27:18 -0500

    Add files via upload

---
+++ Frown/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+public class Frown
+{
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("  ---   ---  ");
+        Console.WriteLine(" |   | |   | ");
+        Console.WriteLine(" |   | |   | ");
+        Console.WriteLine("  ---   ---  ");
+        Console.WriteLine("             ");
+        Console.WriteLine("             ");
+        Console.WriteLine("\\           /");
+        Console.WriteLine(" \\         / ");
+        Console.WriteLine("  \\       /  ");
+        Console.WriteLine("   \\_____/   ");
+        Console.ReadLine();
+    }
+}
